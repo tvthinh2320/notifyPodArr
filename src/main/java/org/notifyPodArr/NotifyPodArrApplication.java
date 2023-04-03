0beb421bbde11444e8a4ca9cebc592d410e0909c
tvthinh2320 <thinhtvts2112016@fpt.edu.vn>
2023-04-03 16:00:50 +0700

    Adding Pom for Jar file and Extends in Application

--- src/main/java/org/notifyPodArr/NotifyPodArrApplication.java
+++ src/main/java/org/notifyPodArr/NotifyPodArrApplication.java
@@ -2,9 +2,11 @@ package org.notifyPodArr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class NotifyPodArrApplication {
+public class NotifyPodArrApplication extends SpringServletContainerInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(NotifyPodArrApplication.class, args);
     }
